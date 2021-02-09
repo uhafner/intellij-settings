e05cc34e4edd783ab48bec7a3ad5ba49b4969d39
Ulli Hafner <ullrich.hafner@gmail.com>
2021-02-09 23:12:02 +0100

    IU-2020.3.1 <hafner@Ullis-MBP-4.fritz.box Create debugger.xml, updates.xml, keymap.xml, editor.xml, projectView.xml, Default.xml, ide.general.xml, project.default.xml, shared-indexes.xml, pluginAdvertiser.xml, Catch Statement Declaration.java, web-browsers.xml, grazie_global.xml, usageView.xml, profiler.xml, ide-features-trainer.xml, Default.xml, javaeeExternalResources.xml, JavaDoc.xml, jdk.table.xml, lightEdit.xml, notifications.xml, filetypes.xml, iterations.xml, equalsHashCodeTemplates.xml, templates.xml, JUnit5 Test Class.java, colors.scheme.xml, databaseDrivers.xml, github.xml, setterTemplates.xml, laf.xml, baseRefactoring.xml, mavenRunHelper.xml, asciidoc.xml, keyboard.xml, KeyPromoterXSettings.xml, intentionSettings.xml, plantuml.xml, toStringTemplates.xml, ui.lnf.xml, checkstyle-idea.xml, path.macros.xml, print.xml, Class.java, Mac OS X 10_5_ copy.xml, vcs.xml, JUnit5 Test Method.java, find.xml, diff.xml, parameter.hints.xml

---
+++ fileTemplates/code/JUnit5 Test Class.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.*;
+#parse("File Header.java")
+/**
+ * Tests the class {@link ${CLASS_NAME}}.
+ *
+ * @author Ullrich Hafner
+ */
+class ${NAME} {
+  ${BODY}
+}
